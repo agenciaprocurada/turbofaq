c976ae5a9fcacebce51e2c6cea4b96662fb4745a
mciciliatti <mariociciliatti@gmail.com>
2026-04-01 10:40:13 -0300

    feat: apply TurboCloud brand theme - primary #42094F, accent #6bdd12, Nunito font, dark header

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <JsonLd data={organizationSchema(SITE_URL)} />
         <JsonLd data={siteLinksSearchBoxSchema(SITE_URL)} />
